6063b07afc12557ffd4c143b17edd3bc9302b398
I-CAT <the_kat690@hotmail.com>
2020-11-17 14:27:22 -0500

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,5 @@
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem Copyright (c) 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%" == "" @echo off
+@if "%DEBUG%" == "false" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
